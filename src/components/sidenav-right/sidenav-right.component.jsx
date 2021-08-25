0dd0fd2257deeba5d977509adb875ad4a0a42b17
Evangel <dev.evangel@gmail.com>
2021-08-24 22:28:58 -1200

    Added right side nav contents

--- src/components/sidenav-right/sidenav-right.component.jsx
+++ src/components/sidenav-right/sidenav-right.component.jsx
@@ -1,11 +1,50 @@
 import React from "react";
+import { FaEllipsisH, FaThLarge, FaMinus, FaTimes } from "react-icons/fa";
 
 import "./sidenav-right.styles.css";
 
 const RightSideNav = () => (
   <nav className="sidenav-right-container">
-    <div className="">
-      <p>stuff goes here...</p>
+    <div className="sidenav-right-menu-icons-container">
+      <p className="sidenav-right-menu-title">Feature 3</p>
+      <button className="sidenav-right-more-icon">
+        <FaEllipsisH />
+      </button>
+      <button className="sidenav-right-grid-icon">
+        <FaThLarge />
+      </button>
+      <button className="sidenav-right-minus-icon">
+        <FaMinus />
+      </button>
+    </div>
+    <div className="sidenav-right-menu-activity">
+      <div className="sidenav-right-activity-title">Tasks due</div>
+      <div className="sidenav-right-task-holder">
+        <div className="task-item">
+          Card title - 08/17/2021 <div class="task-item-board">Feature 3</div>
+        </div>
+        <FaTimes className="task-cancel-icon" />
+      </div>
+      <div className="sidenav-right-task-holder">
+        <div className="task-item">
+          Card title - 08/17/2021 <div class="task-item-board">Feature 3</div>
+        </div>
+        <FaTimes className="task-cancel-icon" />
+      </div>
+      <div className="sidenav-right-task-holder">
+        <div className="task-item">
+          Card title - 08/17/2021 <div class="task-item-board">Feature 3</div>
+        </div>
+        <FaTimes className="task-cancel-icon" />
+      </div>
+      <div className="sidenav-right-task-holder">
+        <div className="task-item">
+          Card title - 08/17/2021 <div class="task-item-board">Feature 3</div>
+        </div>
+        <FaTimes className="task-cancel-icon" />
+      </div>
+
+      <div className="sidenav-right-logout">Log out</div>
     </div>
   </nav>
 );
