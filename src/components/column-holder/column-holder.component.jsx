d7d02eef9a818332a1ef5f0237ffe3a124c3b75e
Evangel <dev.evangel@gmail.com>
2021-08-25 06:19:53 -1200

    Created card component and used it also got app area ready for finishing touches, implemented hold and drag to scroll

--- src/components/column-holder/column-holder.component.jsx
+++ src/components/column-holder/column-holder.component.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 import Column from "../column/column.component";
 
 import "./column-holder.styles.css";
 
 const ColumnHolder = () => (
-  <div className="column-holder">
-    <Column />
-  </div>
+  <ScrollContainer
+    className="scroll-container"
+    vertical={true}
+    horizontal={true}
+  >
+    <div className="column-holder">
+      <Column />
+      <Column />
+      <Column />
+      <Column />
+      <Column />
+      <Column />
+    </div>
+  </ScrollContainer>
 );
 export default ColumnHolder;
