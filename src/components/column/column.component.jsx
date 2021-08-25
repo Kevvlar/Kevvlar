67a170a80cdb55d327e3b4a43a55c4d0225aded8
Evangel <dev.evangel@gmail.com>
2021-08-25 04:09:38 -1200

    Created column component

---
+++ src/components/column/column.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./column.styles.css";
+
+const Column = () => (
+  <div className="column">
+    <p>This is one column</p>
+  </div>
+);
+
+export default Column;
