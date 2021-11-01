8ebdaffd81c9ff904d96f9e5ad4fe1b6f543eb55
Damir Hotlovac <dhotlovac@gmail.com>
2021-10-31 22:22:29 -0500

    Left side menu fixes, clicking board btn open side menu also

--- src/components/board-nav-bar/BoardNavBar.jsx
+++ src/components/board-nav-bar/BoardNavBar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import ScrollContainer from "react-indiana-drag-scroll";
+import { showLeftSideNav } from "../../redux/index";
 
 import "./boardnavbar.css";
 
-const BoardNavBar = ({ boardTitle }) => (
+const BoardNavBar = ({ boardTitle, toggleLeftSideNav }) => (
   <ScrollContainer
       className="scroll-container boardnavbar-scroll-container"
       horizontal={true}
     >
     <div className="boardnavbar">
-      <btn className="boardnavbar-btn">
+      <btn className="boardnavbar-btn" onClick={() => {
+            toggleLeftSideNav();
+          }}>
         <svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M4.66683 12.6665H11.3335V2.6665H4.66683V12.6665ZM1.3335 11.3332H4.00016V3.99984H1.3335V11.3332ZM12.0002 3.99984V11.3332H14.6668V3.99984H12.0002Z" fill="#A0A0A0"/>
         </svg>
@@ -43,4 +46,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(BoardNavBar);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleLeftSideNav: () => dispatch(showLeftSideNav()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoardNavBar);
