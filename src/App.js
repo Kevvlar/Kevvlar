ced84fd28bcd46d167b8f0d44f40ba2b3c9ac2df
Damir Hotlovac <dhotlovac@gmail.com>
2021-11-03 12:33:49 -0500

    change input length

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { Switch, Route } from "react-router-dom";
 
-import Homepage from "./pages/HomePage/HomePage";
-import MainPage from "./pages/MainPage/MainPage";
+import Homepage from "./pages/homepage/HomePage";
+import MainPage from "./pages/mainpage/MainPage";
 import SignUp from "./components/signup/SignUp";
 import SignIn from "./components/signin/SignIn";
 import PrivateRoute from "./components/auth/PrivateRoute";
