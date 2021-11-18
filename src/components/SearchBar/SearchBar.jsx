e5fbf6b7f62fbff6605a8d35bd2d33a801d32fe9
Evangel <dev.evangel@gmail.com>
2021-11-18 23:06:16 +0100

    added search icon

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
+import { FaSearch } from "react-icons/fa";
 
 import { enterSearchText } from "../../redux/index";
 
 import "./searchBar.css";
 
 const SearchBar = ({ inputSearchKey }) => (
-  <input
-    type="text"
-    placeholder="Search"
-    className="boards-search"
-    onChange={(e) => inputSearchKey(e.target.value)}
-  />
+  <div className="search-bar-container">
+    <FaSearch className="search-icon" />
+    <input
+      type="text"
+      placeholder="Search"
+      className="boards-search"
+      onChange={(e) => inputSearchKey(e.target.value)}
+    />
+  </div>
 );
 
 const mapDispatchToProps = (dispatch) => {
