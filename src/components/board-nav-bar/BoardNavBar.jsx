c5abf1fbc7728fcb2dd3c4d6713f188974949fe9
Damir Hotlovac <dhotlovac@gmail.com>
2022-01-07 15:26:11 -0600

    improved boardnavbar grouping

--- src/components/board-nav-bar/BoardNavBar.jsx
+++ src/components/board-nav-bar/BoardNavBar.jsx
@@ -21,49 +21,53 @@ import "./boardnavbar.css";
 
 const BoardNavBar = ({ board, history, showUserModal, emptyColumns, user }) => (
   <div className="boardnavbar">
-    <button
-      className="boardnavbar-btn"
-      onClick={() => {
-        socket.disconnect();
-        emptyColumns();
-        history.push("/boards");
-      }}
-    >
-      <BoardIcon />
-      <div className="boardnavbar-boardtitle">{board.title}</div>
-    </button>
-    <button className="boardnavbar-btn">
-      <LockIcon />
-      <div className="boardnavbar-boardtitle">Private</div>
-    </button>
-    <button className="boardnavbar-btn">
-      <TeamIcon />
-      <div className="boardnavbar-boardtitle">Team Name</div>
-    </button>
-    {board.admins.map((admin, index) => (
-      <AdminAvatar key={index} admin={admin} />
-    ))}
-    {board.members.slice(0, 2).map((member, index) => (
-      <UserAvatar key={index} user={member} />
-    ))}
-    {(board.members.length > 2) ? <div className="avatar-more" title={board.members.slice(2).map((member) => (member.name))}>{("+" + (board.members.length - 2))}</div> : null}
-    <button
-      className="add-user-icon"
-      onClick={() => {
-        showUserModal();
-      }}
-    >
-      <AddUserIcon />
-    </button>
-    <CardSearchBar />
-    <button className="boardnavbar-btn">
-      <ActivityIcon />
-      <div className="boardnavbar-boardtitle">Activity</div>
-    </button>
-    <button className="boardnavbar-btn">
-      <FileshareIcon />
-      <div className="boardnavbar-boardtitle">Files</div>
-    </button>
+    <div className="boardnavbar-group">
+      <button
+        className="boardnavbar-btn"
+        onClick={() => {
+          socket.disconnect();
+          emptyColumns();
+          history.push("/boards");
+        }}
+      >
+        <BoardIcon />
+        <div className="boardnavbar-boardtitle">{board.title}</div>
+      </button>
+      <button className="boardnavbar-btn">
+        <LockIcon />
+        <div className="boardnavbar-boardtitle">Private</div>
+      </button>
+      <button className="boardnavbar-btn">
+        <TeamIcon />
+        <div className="boardnavbar-boardtitle">Team Name</div>
+      </button>
+      {board.admins.map((admin, index) => (
+        <AdminAvatar key={index} admin={admin} />
+      ))}
+      {board.members.slice(0, 2).map((member, index) => (
+        <UserAvatar key={index} user={member} />
+      ))}
+      {(board.members.length > 2) ? <div className="avatar-more" title={board.members.slice(2).map((member) => (member.name))}>{("+" + (board.members.length - 2))}</div> : null}
+      <button
+        className="add-user-icon"
+        onClick={() => {
+          showUserModal();
+        }}
+      >
+        <AddUserIcon />
+      </button>
+    </div>
+    <div className="boardnavbar-group">
+      <CardSearchBar />
+      <button className="boardnavbar-btn">
+        <ActivityIcon />
+        <div className="boardnavbar-boardtitle">Activity</div>
+      </button>
+      <button className="boardnavbar-btn">
+        <FileshareIcon />
+        <div className="boardnavbar-boardtitle">Files</div>
+      </button>
+    </div>
   </div>
 );
 const mapStateToProps = (state) => {
