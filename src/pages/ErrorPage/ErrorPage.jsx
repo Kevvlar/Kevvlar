570c891baade02b7dcd5f17580be2f5d2a2e88c5
Evangel <dev.evangel@gmail.com>
2022-02-16 14:37:42 +0100

    Handled socket connection when internet connection is lost

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { connect } from "react-redux";
 import { LoadingIcon } from "../../assets/svg/iconlibrary";
 import { withRouter } from "react-router-dom";
+
+import socket from "../../Socket";
 
 import "./errorPage.css";
 
 class ErrorPage extends React.Component {
   componentDidMount() {
     window.ononline = (event) => {
+      socket.connect();
+      socket.emit("join-board", this.props.board.id);
       this.props.history.goBack();
     };
   }
@@ -21,4 +26,10 @@ class ErrorPage extends React.Component {
   }
 }
 
-export default withRouter(ErrorPage);
+const mapStateToProps = (state) => {
+  return {
+    board: state.board.selectBoard,
+  };
+};
+
+export default connect(mapStateToProps, null)(withRouter(ErrorPage));
