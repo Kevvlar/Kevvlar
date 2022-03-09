5e54f14d232676c77f0ff1150df7ccbd0365fbb3
Evangel <dev.evangel@gmail.com>
2022-03-09 15:37:54 +0100

    ADD: added basic chat UI

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -3,7 +3,30 @@ import React from "react";
 import "./chatPage.css";
 
 const ChatPage = () => {
-  return <div className="chat-page-container"></div>;
+  return (
+    <div className="chat-page-container">
+      <div className="chat-main">
+        <div className="chat-header">
+          <h4># {"  "}Kevvlar</h4>
+          <h4>Join voice</h4>
+        </div>
+        <div className="chat-area">
+          <h4>Chat Main</h4>
+        </div>
+        <div className="chat-input">
+          <h4>Chat Input</h4>
+        </div>
+      </div>
+      <div className="chat-channel-messages">
+        <div className="channel-list">
+          <h4>Channel List</h4>
+        </div>
+        <div className="direct-message-list">
+          <h4>Direct Messages</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
