44d3b4fa5eb31dff257500df6180e28289c2a365
Evangel <dev.evangel@gmail.com>
2022-04-08 20:29:06 +0100

    ADD: added notification sound

--- src/components/modal/ChatModal/ChatModal.jsx
+++ src/components/modal/ChatModal/ChatModal.jsx
@@ -5,19 +5,25 @@ import { StreamChat } from "stream-chat";
 import ChatPage from "../../../pages/ChatPage/ChatPage";
 import { setChatNotifyOn } from "../../../redux";
 
+import audio from "../../../assets/sound/notify.wav";
+
 import "./chatModal.css";
 
 const client = StreamChat.getInstance(process.env.REACT_APP_STREAM_API_KEY);
+
+const notifySound = new Audio(audio);
 
 class ChatModal extends React.Component {
   componentDidMount() {
     this.newNotification = client.on("message.new", (event) => {
       if (event.user.id !== this.props.user._id) {
         if (event.channel_type === "team") {
+          notifySound.play();
           this.props.turnChatNotificationOn(event.channel_id);
         }
 
         if (event.channel_type === "messaging") {
+          notifySound.play();
           this.props.turnChatNotificationOn(event.user.name);
         }
       }
