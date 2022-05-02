6a184fe2871aa0c233cccb7a5dad2df689eee49e
Evangel <dev.evangel@gmail.com>
2022-05-03 00:20:32 +0100

    Added video Iframe

--- src/pages/ConferencePage/ConferencePage.jsx
+++ src/pages/ConferencePage/ConferencePage.jsx
@@ -8,21 +8,29 @@ import "./conferencePage.css";
 const ConferencePage = ({ minimizeMeeting, closeMeeting }) => {
   return (
     <div className="conference-page">
-      <p>Video Conference Page</p>
-      <button
-        onClick={() => {
-          minimizeMeeting();
-        }}
-      >
-        Minimize
-      </button>
-      <button
-        onClick={() => {
-          closeMeeting();
-        }}
-      >
-        Leave
-      </button>
+      <iframe
+        src="https://video-app-4088-3712-dev.twil.io/?passcode=60809940883712"
+        title="Kevvlar Video Conference"
+        allow="camera;microphone;display-capture;"
+        width="100%"
+        height="100%"
+      />
+      <div className="conference-buttons">
+        <button
+          onClick={() => {
+            minimizeMeeting();
+          }}
+        >
+          Minimize
+        </button>
+        <button
+          onClick={() => {
+            closeMeeting();
+          }}
+        >
+          Leave
+        </button>
+      </div>
     </div>
   );
 };
