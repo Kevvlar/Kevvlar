ecae855b4e44ce0a937c62c440854b5352a0f7ab
Evangel <dev.evangel@gmail.com>
2022-05-05 04:30:02 +0100

    finished up iframe feature

--- src/pages/ConferencePage/ConferencePage.jsx
+++ src/pages/ConferencePage/ConferencePage.jsx
@@ -6,17 +6,29 @@ import { minimizeConferenceModal, closeConferenceModal } from "../../redux";
 
 import "./conferencePage.css";
 
-const ConferencePage = ({ minimizeMeeting, closeMeeting }) => {
+const ConferencePage = ({ minimizeMeeting, closeMeeting, boardId, user }) => {
   return (
     <div className="conference-page">
       <div className="conference-buttons">
-        <button 
-          class="chat-minimize-btn" 
+        <button
+          className="chat-minimize-btn"
           onClick={() => {
             minimizeMeeting();
-          }}>
-          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="10" height="2" transform="matrix(1 0 0 -1 3 9)" fill="#A0A0A0"></rect>
+          }}
+        >
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              width="10"
+              height="2"
+              transform="matrix(1 0 0 -1 3 9)"
+              fill="#A0A0A0"
+            ></rect>
           </svg>
         </button>
         <FaTimes
@@ -27,15 +39,23 @@ const ConferencePage = ({ minimizeMeeting, closeMeeting }) => {
         />
       </div>
       <iframe
-        src="https://video-app-4088-3712-dev.twil.io/?passcode=60809940883712"
+        src="https://video-app-8861-6948-dev.twil.io?passcode=73562688616948"
         title="Kevvlar Video Conference"
         allow="camera;microphone;display-capture;"
         width="100%"
         height="100%"
         className="meeting-iframe"
-      />
+        name={`${boardId}+${user.name}`}
+      ></iframe>
     </div>
   );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    boardId: state.board.selectBoard.id,
+    user: state.user.userData,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
@@ -45,4 +65,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(ConferencePage);
+export default connect(mapStateToProps, mapDispatchToProps)(ConferencePage);
